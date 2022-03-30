deebdf8437a1c59cbee90b1a6a0d648a6e6a92b2
thomasbrasil46 <thomasbrasilpb@gmail.com>
2022-03-30 15:24:34 -0300

    First commit. Stopwatch structure.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void Start ()
+        {
+            int time = 10;
+            int currentTime = 0;
+
+            while (currentTime != time) 
+            {
+                currentTime++;
+            }
+        }
+    }
+}
