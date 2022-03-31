7f80941b372f01090f12917dbe9d3a5d4a468cd1
thomasbrasil46 <thomasbrasilpb@gmail.com>
2022-03-30 21:56:33 -0300

    Finished.

--- Program.cs
+++ Program.cs
@@ -19,11 +19,31 @@ namespace stopwatch
             Console.WriteLine("Quanto tempo deseja contar?");
 
             string data = Console.ReadLine().ToLower();
-            char type = char.Parse(data.Substring(data.Length-1, 1));
-            Console.WriteLine(data);
-            Console.WriteLine(type);
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if(type == 'm')
+                multiplier = 60;
+
+            if(time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multiplier);        
         }
 
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("GO!");
+            Thread.Sleep(2500);
+
+            Start(time);
+        }
         static void Start (int time)
         {
             int currentTime = 0;
@@ -39,6 +59,7 @@ namespace stopwatch
             Console.Clear();
             Console.WriteLine("Stopwatch finalizado.");
             Thread.Sleep(2500);
+            Menu();
         }
     }
 }
